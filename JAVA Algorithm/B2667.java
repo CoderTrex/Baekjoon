07bb491de7e1e89bd1303ed9b9292f42497dcb35
silvercastle <jsilvercastle@gmail.com>
2024-03-02 14:36:13 +0900

    3월 2일 백준

---
+++ JAVA Algorithm/B2667.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class B2667 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+    }
+    
+}
